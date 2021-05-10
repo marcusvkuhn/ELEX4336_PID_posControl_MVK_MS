143ab972e359bb54afe4992760fd896e455a3432
Marcus Kuhn <marcusvkuhn@gmail.com>
2021-05-10 15:10:55 -0700

    added command parser.

--- main.c
+++ main.c
@@ -29,19 +29,22 @@
 #include "UcsControl.h"
 #include "usciUart.h"
 #include "vnh7070API.h"
+#include "cmdParser.h"
+
 
 #define dcoFreq 20 // MHz.
 
 /**
  * main.c
  */
-int main(void){
+void main(void){
 
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
     unsigned char oscFail = 1;
-    char speedStr[30];
-    int clrLenght;
+    char rxString[50];
+//    char speedStr[30];
+//    int clrLenght;
 
     // set clock frequency to 20MHz
     ucsSelSource(1,1,1,1);
@@ -51,7 +54,10 @@ int main(void){
 
 	initEncDec();
 	usciA1UartInit();
+    CMD cmds[MAX_CMDS]; //this is an array of vnh7070Cmds of type CMD
+    initCmds(cmds);
 
+    // set PWM freq
 	timerA0Init(10000);
 
 	// example 180deg
@@ -60,27 +66,35 @@ int main(void){
 	// enable global interrupts (SR.GIE bit)
     __enable_interrupt();
 
-	while(1){
+
+    int cmdIndex;
+
+    while(1){
 	    __delay_cycles(1000000);            // 50ms delay
 	    pidControlLoop(2, 1, 1, 0.01); // Kp, Kd, Ki, dt
-	    //if (screenWrite == 1){
-        for (clrLenght = 40; clrLenght != 0; --clrLenght)
-            usciA1UartTxChar(' ');                          // this loop clears the console
 
-        usciA1UartTxChar('\r');                         // returns to beginning of line
+        do{
+            usciA1UartGets(rxString);
+            cmdIndex = parseCmd(cmds, rxString);
+            if (cmdIndex != -1){
+                if (cmdIndex != QUIT_IDX){
+                    executeCmd(cmds, cmdIndex);
+                }
+            }
+            else
+                usciA1UartTxString("Invalid command.");
+        } while (cmdIndex != QUIT_IDX);
 
-//        angSpeedDen = (pulseDelta*0.01) * 1150;
-//        angSpeed = angSpeedNum / angSpeedDen;
-//          angSpeed = (SMCLK*2*M_PI)/(pulseDelta*P_REV_Q);
-        sprintf(speedStr, "posCount = %d, command = %f", posCount, controlCmd);
-        usciA1UartTxString(speedStr);
-        //speedCalc = 0;
 
-        //            usciB1SpiTxBuffer(&posCount, 2);
-
-        screenWrite = 0;                                // reset enabler to write data to screen
-
+//        if (screenWrite == 1){
+//        for (clrLenght = 40; clrLenght != 0; --clrLenght)
+//            usciA1UartTxChar(' ');                          // this loop clears the console
+//
+//        usciA1UartTxChar('\r');                         // returns to beginning of line
+//
+//        sprintf(speedStr, "posCount = %d, command = %f", posCount, controlCmd);
+//        usciA1UartTxString(speedStr);
+//
+//        screenWrite = 0;                                // reset enabler to write data to screen
 	}
-
-	return 0;
 }
